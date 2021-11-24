59cdf73ec2e724145d76884ffc7102c1f07b66f8
Miguel Sedek <miguelsedek@gmail.com>
2021-11-24 12:43:34 -0300

    Update Constants.java

--- src/main/java/com/globallogic/userapi/entities/Constants.java
+++ src/main/java/com/globallogic/userapi/entities/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String MAIL_REGEX = "[a-z]+@[a-z]+\\.[cl]{2}";
     public static final String INVALID_MAIL = "Bad request invalid email format";
 
-    public static final String PASSWORD_REGEX = "^(?!(?:[^0-9]*[0-9]){3})(?=^(?:\\D*\\d\\D*){2}$)(?=.*[a-z])[a-z0-9]*[A-Z][a-z0-9]*$";
+    public static final String PASSWORD_REGEX = "^(?!(?:[^0-9]*[0-9]){3})(?=^(?:\D*\d\D*){2}$)(?=.*[a-z])[a-z0-9]*[A-Z][a-z0-9]*$";
     public static final String INVALID_PASSWORD = "Bad request invalid password, make sure is 1 Uppercase letter, 2 digits and N lowercase letters";
 
     public static final String EMAIL_EXCEPTION_KEYWORD = "EMAIL";
