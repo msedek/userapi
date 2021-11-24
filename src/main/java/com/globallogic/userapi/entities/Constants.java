5305563741929d95ebf5f58bf2e91bf911e879d3
Miguel Sedek <miguelsedek@gmail.com>
2021-11-24 12:44:29 -0300

    Update Constants.java

--- src/main/java/com/globallogic/userapi/entities/Constants.java
+++ src/main/java/com/globallogic/userapi/entities/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String NO_BODY = "Bad request no body sent";
     public static final String MISSING_INFO = "Bad request missing information";
 
-    public static final String MAIL_REGEX = "[a-z]+@[a-z]+\\.[cl]{2}";
+    public static final String MAIL_REGEX = "[a-z]+@[a-z]+\.[cl]{2}";
     public static final String INVALID_MAIL = "Bad request invalid email format";
 
     public static final String PASSWORD_REGEX = "^(?!(?:[^0-9]*[0-9]){3})(?=^(?:\D*\d\D*){2}$)(?=.*[a-z])[a-z0-9]*[A-Z][a-z0-9]*$";
